8f1aa35b636fe6d91abef6719cef752140ff3698
Ante Petrovic <petrovicante6@gmail.com>
2025-11-05 17:23:45 +0100

    add hyprlock

---
+++ modules/features/hyprlock/default.nix
@@ -0,0 +1,10 @@
+{ inputs, self, ...}: 
+{
+  flake.homeManagerModules.hyprlock = { config, lib, pkgs, ...}: {
+    imports = [
+      self.homeModules.hyprlock
+    ];
+
+    magos.hm.hyprlock.enable = lib.mkDefault true;
+  };
+}
