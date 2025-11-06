3fdc5cfa93b283af68f02c6573d56e87aa67f381
Ante Petrovic <petrovicante6@gmail.com>
2025-11-06 16:22:10 +0100

    remove unneeded variables

--- modules/core/walker/default.nix
+++ modules/core/walker/default.nix
@@ -11,19 +11,7 @@
     ...
   }: let
     inherit (lib) mkIf mkEnableOption;
-    #  c = config.lib.stylix.colors ;
     c = config.magos.palette;
-    # backgroundDefault = "#${c.base00}";
-    # backgroundAlpha50 = "alpha(#${c.base01}, 0.5)";
-    # background        = "#${c.base01}";
-    # foreground        = "#${c.base05}";
-    # textDefault       = "#${c.base05}";
-    # textAlternate     = "#${c.base04}";
-    # textPopup         = "#${c.base0A}";
-    # border            = "#${c.base0D}";
-    # warning           = "#${c.base0A}";
-    # urgent            = "#${c.base09}";
-    # error             = "#${c.base08}";
   in {
     imports = [
       ../../theme/palette.nix
